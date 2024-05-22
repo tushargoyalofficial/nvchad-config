39ea023aebb9b7e6da27895af5563a8aa9c92d39
Tushar Goyal <tushar462024@gmail.com>
2024-05-22 10:55:28 +0530

    Code lint

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,7 @@ local opt = vim.opt
 opt.title = true
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.cursorlineopt = "both"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
